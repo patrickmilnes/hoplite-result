2db0069cd05add14ae3c601a6c7bb806b83bb208
patrickmilnes <pjamilnes@gmail.com>
2025-04-26 19:06:42 +0100

    Removed private setter for trace id in `TraceResult`

--- src/HopliteLabs.Result/HopliteLabs.Result.Core/TraceResult.cs
+++ src/HopliteLabs.Result/HopliteLabs.Result.Core/TraceResult.cs
@@ -2,12 +2,12 @@
 
 public class TraceResult<T, TS> : Result<T, TS> where TS : IError
 {
-    public Guid TraceId { get; private set; }
+    public Guid TraceId { get; set; }
 
     public TraceResult(bool isOk, Guid traceId, T? value, TS? error) : base(isOk, value, error) => TraceId = traceId;
 
     public Result<T, TS> ToResult() => this;
 
-    public static TraceResult<T, TS> Ok(Guid traceId, T value) => new (true, traceId, value, default);
-    public static TraceResult<T, TS> Err(Guid traceId, TS error) => new (false, traceId, default, error);
+    public static TraceResult<T, TS> Ok(Guid traceId, T value) => new(true, traceId, value, default);
+    public static TraceResult<T, TS> Err(Guid traceId, TS error) => new(false, traceId, default, error);
 }
